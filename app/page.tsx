cf8fd286fb2f1d64f8e5b87d8de080b056172521
erin <erin.gwc@gmail.com>
2022-11-16 17:09:18 +0900

    Create pages for root navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return <h1 className="relative text-3xl font-bold underline">Yo!</h1>;
---
+++ app/readings/page.tsx
@@ -0,0 +1,3 @@
+export default function Readings() {
+  return <div>Readings</div>;
+}
